1729376d928c8f9ed8a517856b51bdde0fdc51dd
mourasnts <ws.mourax@gmail.com>
2021-05-10 20:41:29 -0300

    .

--- next.config.js
+++ next.config.js
@@ -1,12 +1,12 @@
 module.exports = {
     env: {
-        "BASE_URL": "http://localhost:3000",
-        "MONGODB_URL": "YOUR_MONGODB_URL",
-        "ACCESS_TOKEN_SECRET": "YOUR_ACCESS_TOKEN_SECRET",
-        "REFRESH_TOKEN_SECRET": "YOUR_REFRESH_TOKEN_SECRET",
+        "BASE_URL": "https://nextjs-ecommerce-orpin.vercel.app",
+        "MONGODB_URL": "mongodb+srv://DbUser:ZuMR37pS6nk-4s9@cluster0.nq63f.mongodb.net/commerce",
+        "ACCESS_TOKEN_SECRET": "T0k3nDeAc3sS0",
+        "REFRESH_TOKEN_SECRET": "t0kEnD3R3fResh",
         "PAYPAL_CLIENT_ID": "YOUR_PAYPAL_CLIENT_ID",
         "CLOUD_UPDATE_PRESET": "YOUR_CLOUD_UPDATE_PRESET",
-        "CLOUD_NAME": "YOUR_CLOUD_NAME",
-        "CLOUD_API": "YOUR_CLOUD_API"
+        "CLOUD_NAME": "commerce16072001",
+        "CLOUD_API": "483969158735316"
     }
 }
